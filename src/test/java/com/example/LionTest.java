b5cdaac2e7ae44e87e5ea8efaa48e9b95caad411
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2022-11-16 17:32:41 +0400

    Preparation for test creation

---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+public class LionTest {
+    
+}
