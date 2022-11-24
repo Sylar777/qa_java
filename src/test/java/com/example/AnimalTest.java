4a8c936e5b9a9533908084b1b6b5757afcfcd22e
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2022-11-24 19:43:09 +0400

    Add one more test for Animal Class

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -3,16 +3,30 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AnimalTest {
 
+    Animal animal;
+
+    @Before
+    public void setUp() {
+        animal = new Animal();
+    }
+
     @Test
     public void getFood() {
         Exception actualExeption = assertThrows(Exception.class, () ->
-                new Animal().getFood("Неизвестный вид животного"));
+        animal.getFood("Неизвестный вид животного"));
 
         assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", 
                 actualExeption.getMessage());
+    }
+
+    @Test
+    public void getFamily(){
+        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи",
+        animal.getFamily());
     }
 }
